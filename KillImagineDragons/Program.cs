063e32c07e77bcb4ef44e53517714b911df82bc6
Shane D Wilson <me@shanedwilson.com>
2019-03-12 20:56:52 -0500

    working

--- KillImagineDragons/Program.cs
+++ KillImagineDragons/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KillImagineDragons
 {
@@ -6,7 +8,32 @@ namespace KillImagineDragons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            List<Song> goodSongs = new List<Song>();
+            List<Song> allSongs = new List<Song>
+            {
+                new Song("Imagine Dragons", "Bleeding Out"),
+                new Song("Imagine Dragons", "It's Time"),
+                new Song("Imagine Dragons", "Radioactive"),
+                new Song("U2", "Sunday Bloody Sunday"),
+                new Song("U2", "Pride(In The Name Of Love)"),
+                new Song("Tom Petty", "Last Dance With Mary Jane"),
+                new Song("Tom Petty", "It's Good To Be King"),
+                new Song("Johnny Cash", "Hurt"),
+                new Song("Johnny Cash", "Hurt"),
+                new Song("Johnny Cash", "Ain't No Grave"),
+                new Song("Josh Ritter", "Girl In The War"),
+                new Song("Buddy Miller", "With God On Our Side"),
+                new Song("Lucinda Williams", "Essence")
+        };
+
+            goodSongs = allSongs
+                        .Where(song => song.Artist != "Imagine Dragons").ToList();
+
+            foreach(Song song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist} : {song.Name}.");
+            }
+            Console.ReadKey();                        
+         }
     }
 }
